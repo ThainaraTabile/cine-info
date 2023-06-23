305b4bb8e33ea44d7749a53997011ba51f2063a4
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-06-23 16:23:49 -0300

    exclui comentários

--- src/api/config-api.js
+++ src/api/config-api.js
@@ -1,17 +1,3 @@
-// const urlApi = 'https://api-filmes-rose.vercel.app';
-
-// export const obterListaDeFilmes = async () => {
-//     const response = await fetch(`${urlApi}/movies`, {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//     });
-
-//     return response.json();
-// }
-
-
 const urlApi = process.env.REACT_APP_API_URL;
 
 export const obterListaDeFilmes = async () => {
