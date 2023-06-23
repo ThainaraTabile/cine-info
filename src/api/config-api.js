4ce9bfca07b903b6e91c53f9674411ed611d2fe2
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-06-23 16:21:53 -0300

    implementa var de ambiente

--- src/api/config-api.js
+++ src/api/config-api.js
@@ -1,4 +1,18 @@
-const urlApi = 'https://api-filmes-rose.vercel.app';
+// const urlApi = 'https://api-filmes-rose.vercel.app';
+
+// export const obterListaDeFilmes = async () => {
+//     const response = await fetch(`${urlApi}/movies`, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//     });
+
+//     return response.json();
+// }
+
+
+const urlApi = process.env.REACT_APP_API_URL;
 
 export const obterListaDeFilmes = async () => {
     const response = await fetch(`${urlApi}/movies`, {
