53e6a4edfa79c0953d9c4b3be35c4f212981031e
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-06-22 22:38:13 -0300

    implementa filtro de busca por gênero

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -31,7 +31,8 @@ import { FaStar } from 'react-icons/fa'
 // export default Card;
 
 
-const Card = () => {
+
+const Card = ({ generoSelecionado }) => {
     const [filmes, setFilmes] = useState([]);
 
     useEffect(() => {
@@ -39,7 +40,6 @@ const Card = () => {
             try {
                 const filmesData = await obterListaDeFilmes();
                 setFilmes(filmesData);
-
             } catch (error) {
                 console.error('Erro ao obter filmes!');
             }
@@ -47,24 +47,30 @@ const Card = () => {
         buscarFilmes();
     }, []);
 
+    const filmesFiltrados =
+        generoSelecionado && generoSelecionado !== 'todos'
+            ? filmes.filter((filme) => filme.genre === generoSelecionado)
+            : filmes;
+
     return (
         <section className={styles.container}>
             <div className={styles.colunas}>
-                {filmes.map((filme) => (
+                {filmesFiltrados.map((filme) => (
                     <div className={styles.cardsFilme} key={filme.title}>
                         <img src={filme.image} alt={filme.title} className={styles.imagem} />
                         <div className={styles.titulo}>
                             <h2>{filme.title}</h2>
                         </div>
-                        <span className={styles.span}>
-                            {filme.description}
-                        </span>
+                        <div className={styles.descricao}>
+                            <p>{filme.description}</p>
+                            <p>Genêro: {filme.genre}</p>
+                        </div>
                         <div className={styles.info}>
                             <span>
-                                <FaStar /> {filme.rating}</span>
-                            <span>{filme.duration}</span>
+                                <FaStar /> {filme.rating}
+                            </span>
+                            <span>Duração:{filme.duration}</span>
                         </div>
-
                     </div>
                 ))}
             </div>
--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -3,19 +3,22 @@ import Card from '../../componentes/Card';
 import Menu from '../../componentes/Menu';
 import Pesquisar from '../../componentes/Pesquisar';
 import Rodape from '../../componentes/Rodape';
+import React, { useState } from 'react';
 
 function Inicio() {
+  const [generoSelecionado, setGeneroSelecionado] = useState(null);
+
+  const handleSelecionarGenero = (genero) => {
+    setGeneroSelecionado(genero);
+  };
+
   return (
     <>
-
       <Cabecalho />
       <Pesquisar />
-      <Menu />
-
-      <Card />
+      <Menu aoSelecionarGenero={handleSelecionarGenero} />
+      <Card generoSelecionado={generoSelecionado} />
       <Rodape />
-
-
     </>
   );
 }
