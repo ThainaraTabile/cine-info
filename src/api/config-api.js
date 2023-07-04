32b9886323c349d85892fc0688886a3c5aede100
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-07-04 14:40:28 -0300

    implementa msg de sucesso e erro ao pesquisar titulo

--- src/api/config-api.js
+++ src/api/config-api.js
@@ -1,16 +1,4 @@
 const urlApi = process.env.REACT_APP_API_URL_BASE;
-const urlDeBusca = process.env.REACT_APP_API_URL_TITULO;
-
-export const obterListaDeFilmes = async () => {
-    const response = await fetch(`${urlApi}/movies`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-
-    return response.json();
-}
 
 export const obterFilmes = async ({ titulo, genero }) => {
     const filtro = new URLSearchParams();
@@ -25,17 +13,6 @@ export const obterFilmes = async ({ titulo, genero }) => {
     console.log(filtro)
     const url = `${urlApi}/movies${filtro.size > 0 ? `?${filtro.toString()}` : ''}`
     const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-
-    return response.json();
-}
-
-export const filtrarPorTitulo = async (tituloInput) => {
-    const response = await fetch(`${urlDeBusca}${tituloInput}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
--- src/componentes/Pesquisar/index.js
+++ src/componentes/Pesquisar/index.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 function Pesquisar({ aoBuscarPorTitulo }) {
     const [tituloInput, setTituloInput] = useState('');
+
     const capitalizeTexto = (texto) => {
         const palavras = texto.split(' ');
         const palavrasFormatadas = palavras.map((palavra) => {
@@ -21,7 +22,15 @@ function Pesquisar({ aoBuscarPorTitulo }) {
         aoBuscarPorTitulo(tituloInput);
         setTituloInput('');
     };
+    // const exibirResultado = (titulo) => {
+    //     const mensagem = `Esse é o resultado para: ${titulo}`;
+    //     setMensagemResultado(mensagem);
+    // };
 
+    // const exibirMsgNenhumResultado = (titulo) => {
+    //     const mensagem = `Ops, não encontramos nenhum filme com o título ${titulo}.`;
+    //     setMensagemResultado(mensagem);
+    // };
     return (
         <>
             <div className={styles.pesquisar}>
@@ -46,94 +55,3 @@ function Pesquisar({ aoBuscarPorTitulo }) {
 }
 
 export default Pesquisar;
-
-
-
-
-// import styles from './Pesquisar.module.css';
-// import { BiSearchAlt2 } from 'react-icons/bi'
-// import { useState } from 'react';
-// import { filtrarPorTitulo } from '../../api/config-api';
-
-// import FilmesFiltrados from '../FilmesFiltrados';
-
-// function Pesquisar({ filmes }) {
-//     const [tituloInput, setTituloInput] = useState('');
-//     const [filmesFiltrados, setFilmesFiltrados] = useState([]);
-//     const [mensagemResultado, setMensagemResultado] = useState('');
-
-//     const capitalizeTexto = (texto) => {
-//         const palavras = texto.split(' ');
-//         const palavrasFormatadas = palavras.map((palavra) => {
-//             return palavra.charAt(0).toUpperCase() + palavra.slice(1).toLowerCase();
-//         });
-//         return palavrasFormatadas.join(' ');
-//     };
-
-//     const handleChange = (e) => {
-//         const textoFormatado = capitalizeTexto(e.target.value);
-//         setTituloInput(textoFormatado);
-//     };
-
-//     const exibirResultado = (titulo) => {
-//         const mensagem = `Esse é o resultado para: ${titulo}`;
-//         setMensagemResultado(mensagem);
-//     };
-
-//     const exibirMensagemNenhumResultado = (titulo) => {
-//         const mensagem = `Ops, não encontramos nenhum filme com o título ${titulo}.`;
-//         setMensagemResultado(mensagem);
-//     };
-
-//     const buscarFilmesPorTitulo = async () => {
-//         try {
-//             const filmesData = await filtrarPorTitulo(tituloInput);
-//             if (filmesData.length > 0) {
-//                 setFilmesFiltrados(filmesData);
-//                 exibirResultado(tituloInput);
-//             } else {
-//                 setFilmesFiltrados([]);
-//                 exibirMensagemNenhumResultado(tituloInput);
-//             }
-//         } catch (error) {
-//             console.error('Erro ao obter filmes!');
-//         }
-//     };
-
-//     const aoEnviar = (e) => {
-//         e.preventDefault();
-//         if (!tituloInput) return;
-//         buscarFilmesPorTitulo();
-//         setTituloInput('');
-//     };
-
-//     return (
-//         <>
-//             <div className={styles.pesquisar}>
-//                 <form onSubmit={aoEnviar}>
-//                     <input
-//                         className={styles.input}
-//                         placeholder='Pesquisar pelo título'
-//                         onChange={handleChange}
-//                         value={tituloInput}
-//                     />
-//                     <button type='submit' className={styles.btnIcon}>
-//                         <span className={styles.icon}>
-//                             <BiSearchAlt2 size={30} />
-//                         </span>
-//                     </button>
-//                 </form>
-//             </div>
-//             <div className={styles.resultadoPesquisa}>
-//                 {mensagemResultado && <h2 className={styles.msgResultado}>{mensagemResultado}</h2>}
-//                 {filmes.map((filme) =>
-//                     <FilmesFiltrados key={filme.title} filme={filmesFiltrados} />
-//                 )}
-
-//             </div>
-//         </>
-
-//     );
-// }
-
-// export default Pesquisar;
